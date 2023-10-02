2e545159cb3a903cb66d9c6d68fb9992a43c4cdb
Awan2001 <arifeducation2001@gmail.com>
2023-10-03 00:04:32 +0800

    deletionSmash

--- Smash/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-V7/RnAB1u8qE4p+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-HvQOcvzj+q+1u9BSixQWvxvKc6TJfGJ1l9+u5+i6f0vI1cPh9F80ql+Yf2+5u3/w" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
